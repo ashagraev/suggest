03fc4f18d85cb7b3c71b2a2bed9d2a212f8e15e7
UshakovN <ushakovnika@mpei.ru>
2022-09-08 00:14:24 +0300

    remove tabs

--- handler.go
+++ handler.go
@@ -1,136 +1,136 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"github.com/microcosm-cc/bluemonday"
-	"log"
-	stpb "main/proto/suggest/suggest_trie"
-	"math"
-	"net/http"
-	"net/url"
-	"strconv"
+  "bytes"
+  "encoding/json"
+  "fmt"
+  "github.com/microcosm-cc/bluemonday"
+  "log"
+  stpb "main/proto/suggest/suggest_trie"
+  "math"
+  "net/http"
+  "net/url"
+  "strconv"
 )
 
 type Handler struct {
-	Suggest              *stpb.SuggestData
-	Policy               *bluemonday.Policy
-	EqualShapedNormalize bool
+  Suggest              *stpb.SuggestData
+  Policy               *bluemonday.Policy
+  EqualShapedNormalize bool
 }
 
 func writeCORSHeaders(w http.ResponseWriter) {
-	w.Header().Add("Access-Control-Allow-Origin", "*")
-	w.Header().Add("Access-Control-Allow-Methods", "OPTIONS,POST,GET")
+  w.Header().Add("Access-Control-Allow-Origin", "*")
+  w.Header().Add("Access-Control-Allow-Methods", "OPTIONS,POST,GET")
 }
 
 func reportServerError(w http.ResponseWriter, message string) {
-	writeCORSHeaders(w)
-	w.WriteHeader(http.StatusInternalServerError)
-	if _, err := w.Write([]byte(message)); err != nil {
-		log.Printf("cannot write a message: %v", err)
-	}
+  writeCORSHeaders(w)
+  w.WriteHeader(http.StatusInternalServerError)
+  if _, err := w.Write([]byte(message)); err != nil {
+    log.Printf("cannot write a message: %v", err)
+  }
 }
 
 func reportSuccessMessage(w http.ResponseWriter, message string) {
-	writeCORSHeaders(w)
-	w.WriteHeader(http.StatusOK)
-	if _, err := w.Write([]byte(message)); err != nil {
-		log.Printf("cannot write a message: %v", err)
-	}
+  writeCORSHeaders(w)
+  w.WriteHeader(http.StatusOK)
+  if _, err := w.Write([]byte(message)); err != nil {
+    log.Printf("cannot write a message: %v", err)
+  }
 }
 
 func reportSuccessData(w http.ResponseWriter, data interface{}) {
-	j, err := json.Marshal(data)
-	if err != nil {
-		reportServerError(w, fmt.Sprintf("%v", err))
-		return
-	}
-	var b bytes.Buffer
-	if err := json.Indent(&b, j, "", "  "); err != nil {
-		reportServerError(w, fmt.Sprintf("%v", err))
-		return
-	}
-	writeCORSHeaders(w)
-	w.WriteHeader(http.StatusOK)
-	if _, err := w.Write(b.Bytes()); err != nil {
-		log.Printf("cannot write a message: %v", err)
-	}
+  j, err := json.Marshal(data)
+  if err != nil {
+    reportServerError(w, fmt.Sprintf("%v", err))
+    return
+  }
+  var b bytes.Buffer
+  if err := json.Indent(&b, j, "", "  "); err != nil {
+    reportServerError(w, fmt.Sprintf("%v", err))
+    return
+  }
+  writeCORSHeaders(w)
+  w.WriteHeader(http.StatusOK)
+  if _, err := w.Write(b.Bytes()); err != nil {
+    log.Printf("cannot write a message: %v", err)
+  }
 }
 
 func (h *Handler) HandleHealthRequest(w http.ResponseWriter, _ *http.Request) {
-	reportSuccessMessage(w, "OK")
+  reportSuccessMessage(w, "OK")
 }
 
 type PagingParameters struct {
-	Count        int
-	Page         int
-	PaginationOn bool
+  Count        int
+  Page         int
+  PaginationOn bool
 }
 
 func NewPagingParameters(query url.Values) *PagingParameters {
-	pagingParameters := &PagingParameters{}
-	if count, err := strconv.ParseInt(query.Get("count"), 10, 64); err == nil { // no err
-		pagingParameters.Count = int(count)
-	}
-	if page, err := strconv.ParseInt(query.Get("page"), 10, 64); err == nil { // no err
-		pagingParameters.Page = int(page)
-		pagingParameters.PaginationOn = true
-	}
-	return pagingParameters
+  pagingParameters := &PagingParameters{}
+  if count, err := strconv.ParseInt(query.Get("count"), 10, 64); err == nil { // no err
+    pagingParameters.Count = int(count)
+  }
+  if page, err := strconv.ParseInt(query.Get("page"), 10, 64); err == nil { // no err
+    pagingParameters.Page = int(page)
+    pagingParameters.PaginationOn = true
+  }
+  return pagingParameters
 }
 
 func (pp *PagingParameters) Apply(suggestions []*SuggestAnswerItem) *PaginatedSuggestResponse {
-	pagesCount := 1
-	if pp.Count != 0 {
-		pagesCount = int(math.Ceil(float64(len(suggestions)) / float64(pp.Count)))
-	}
-	itemsCount := len(suggestions)
-	if pp.Page != 0 && pp.Count != 0 {
-		skip := pp.Page * pp.Count
-		if len(suggestions) > skip {
-			suggestions = suggestions[skip:]
-		} else {
-			suggestions = nil
-		}
-	}
-	if pp.Count != 0 && len(suggestions) > pp.Count {
-		suggestions = suggestions[:pp.Count]
-	}
-	return &PaginatedSuggestResponse{
-		Suggestions:     suggestions,
-		PageNumber:      pp.Page,
-		TotalPagesCount: pagesCount,
-		TotalItemsCount: itemsCount,
-	}
+  pagesCount := 1
+  if pp.Count != 0 {
+    pagesCount = int(math.Ceil(float64(len(suggestions)) / float64(pp.Count)))
+  }
+  itemsCount := len(suggestions)
+  if pp.Page != 0 && pp.Count != 0 {
+    skip := pp.Page * pp.Count
+    if len(suggestions) > skip {
+      suggestions = suggestions[skip:]
+    } else {
+      suggestions = nil
+    }
+  }
+  if pp.Count != 0 && len(suggestions) > pp.Count {
+    suggestions = suggestions[:pp.Count]
+  }
+  return &PaginatedSuggestResponse{
+    Suggestions:     suggestions,
+    PageNumber:      pp.Page,
+    TotalPagesCount: pagesCount,
+    TotalItemsCount: itemsCount,
+  }
 }
 
 func (h *Handler) HandleSuggestRequest(w http.ResponseWriter, r *http.Request) {
-	writeCORSHeaders(w)
-	part := r.URL.Query().Get("part")
-	if h.EqualShapedNormalize {
-		part = ToEqualShapedLatin(part)
-	}
-	normalizedPart := part
-	if h.EqualShapedNormalize {
-		normalizedPart = EqualShapedNormalizeString(part, h.Policy)
-	} else {
-		normalizedPart = NormalizeString(part, h.Policy)
-	}
-	antiClasses := r.URL.Query()["anti-class"]
-	antiClassesMap := PrepareCheckMap(antiClasses)
-	classes := r.URL.Query()["class"]
-	classesMap := PrepareCheckMap(classes)
-	suggestions := GetSuggest(h.Suggest, part, normalizedPart, classesMap, antiClassesMap)
-	pagingParameters := NewPagingParameters(r.URL.Query())
-	if pagingParameters.PaginationOn {
-		reportSuccessData(w, pagingParameters.Apply(suggestions))
-	} else {
-		if count, err := strconv.ParseInt(r.URL.Query().Get("count"), 10, 64); err == nil { // no err
-			if count != 0 && len(suggestions) > int(count) {
-				suggestions = suggestions[:count]
-			}
-		}
-		reportSuccessData(w, suggestions)
-	}
+  writeCORSHeaders(w)
+  part := r.URL.Query().Get("part")
+  if h.EqualShapedNormalize {
+    part = ToEqualShapedLatin(part)
+  }
+  normalizedPart := part
+  if h.EqualShapedNormalize {
+    normalizedPart = EqualShapedNormalizeString(part, h.Policy)
+  } else {
+    normalizedPart = NormalizeString(part, h.Policy)
+  }
+  antiClasses := r.URL.Query()["anti-class"]
+  antiClassesMap := PrepareCheckMap(antiClasses)
+  classes := r.URL.Query()["class"]
+  classesMap := PrepareCheckMap(classes)
+  suggestions := GetSuggest(h.Suggest, part, normalizedPart, classesMap, antiClassesMap)
+  pagingParameters := NewPagingParameters(r.URL.Query())
+  if pagingParameters.PaginationOn {
+    reportSuccessData(w, pagingParameters.Apply(suggestions))
+  } else {
+    if count, err := strconv.ParseInt(r.URL.Query().Get("count"), 10, 64); err == nil { // no err
+      if count != 0 && len(suggestions) > int(count) {
+        suggestions = suggestions[:count]
+      }
+    }
+    reportSuccessData(w, suggestions)
+  }
 }
