8e2ad0d59bce8e0960f6352a5ab62b78c00051d8
UshakovN <ushakovnika@mpei.ru>
2022-09-08 00:06:01 +0300

    add anti-class for manual items

---
+++ common.go
@@ -0,0 +1,13 @@
+package main
+
+import "strings"
+
+func PrepareCheckMap(values []string) map[string]bool {
+	valuesMap := map[string]bool{}
+	for _, value := range values {
+		if value != "" {
+			valuesMap[strings.ToLower(value)] = true
+		}
+	}
+	return valuesMap
+}
