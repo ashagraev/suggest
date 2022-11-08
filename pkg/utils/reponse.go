ded89a531c2909d9230c019e79c837fb2031ffab
UshakovN <ushakovnika@mpei.ru>
2022-11-08 12:20:32 +0300

    remove tabs

--- pkg/utils/reponse.go
+++ pkg/utils/reponse.go
@@ -1,57 +1,57 @@
 package utils
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
+  "bytes"
+  "encoding/json"
+  "fmt"
+  "log"
+  "net/http"
 )
 
 func WriteCORSHeaders(w http.ResponseWriter) {
-	w.Header().Add("Access-Control-Allow-Origin", "*")
-	w.Header().Add("Access-Control-Allow-Methods", "OPTIONS,POST,GET")
+  w.Header().Add("Access-Control-Allow-Origin", "*")
+  w.Header().Add("Access-Control-Allow-Methods", "OPTIONS,POST,GET")
 }
 
 func ReportServerError(w http.ResponseWriter, message string) {
-	WriteCORSHeaders(w)
-	w.WriteHeader(http.StatusInternalServerError)
-	if _, err := w.Write([]byte(message)); err != nil {
-		log.Printf("cannot write a message: %v", err)
-	}
+  WriteCORSHeaders(w)
+  w.WriteHeader(http.StatusInternalServerError)
+  if _, err := w.Write([]byte(message)); err != nil {
+    log.Printf("cannot write a message: %v", err)
+  }
 }
 
 func ReportRequestError(w http.ResponseWriter, message string) {
-	WriteCORSHeaders(w)
-	w.WriteHeader(http.StatusBadRequest)
-	if _, err := w.Write([]byte(message)); err != nil {
-		log.Printf("cannot write a message: %v", err)
-	}
+  WriteCORSHeaders(w)
+  w.WriteHeader(http.StatusBadRequest)
+  if _, err := w.Write([]byte(message)); err != nil {
+    log.Printf("cannot write a message: %v", err)
+  }
 }
 
 func ReportSuccessMessage(w http.ResponseWriter, message string) {
-	WriteCORSHeaders(w)
-	w.WriteHeader(http.StatusOK)
-	if _, err := w.Write([]byte(message)); err != nil {
-		log.Printf("cannot write a message: %v", err)
-	}
+  WriteCORSHeaders(w)
+  w.WriteHeader(http.StatusOK)
+  if _, err := w.Write([]byte(message)); err != nil {
+    log.Printf("cannot write a message: %v", err)
+  }
 }
 
 func ReportSuccessData(w http.ResponseWriter, data interface{}) {
-	j, err := json.Marshal(data)
-	if err != nil {
-		ReportServerError(w, fmt.Sprintf("%v", err))
-		return
-	}
-	var b bytes.Buffer
-	if err := json.Indent(&b, j, "", "  "); err != nil {
-		ReportServerError(w, fmt.Sprintf("%v", err))
-		return
-	}
-	WriteCORSHeaders(w)
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	if _, err := w.Write(b.Bytes()); err != nil {
-		log.Printf("cannot write a message: %v", err)
-	}
+  j, err := json.Marshal(data)
+  if err != nil {
+    ReportServerError(w, fmt.Sprintf("%v", err))
+    return
+  }
+  var b bytes.Buffer
+  if err := json.Indent(&b, j, "", "  "); err != nil {
+    ReportServerError(w, fmt.Sprintf("%v", err))
+    return
+  }
+  WriteCORSHeaders(w)
+  w.Header().Add("Content-Type", "application/json")
+  w.WriteHeader(http.StatusOK)
+  if _, err := w.Write(b.Bytes()); err != nil {
+    log.Printf("cannot write a message: %v", err)
+  }
 }
